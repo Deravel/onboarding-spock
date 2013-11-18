6730f8909023bb46397a314155c11499c995c296
Greg Henry <gphenry@gmail.com>
2013-11-18 14:59:00 -0600

    Adding Mockito test implementations for _3InvocationResponseMockito.shouldOptionallyThrowExceptionBasedOnInputArguments and shouldUseArgumentsToCalculateResponse.

--- src/test/groovy/com/example/interaction/_3InvocationResponseMockito.groovy
+++ src/test/groovy/com/example/interaction/_3InvocationResponseMockito.groovy
@@ -5,10 +5,22 @@ import com.example.Service
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.AdditionalAnswers
+import org.mockito.ArgumentMatcher
 import org.mockito.Mock
+import org.mockito.invocation.InvocationOnMock
 import org.mockito.runners.MockitoJUnitRunner
+import org.mockito.stubbing.Answer
 
 import static org.junit.Assert.fail
+import static org.mockito.AdditionalAnswers.returnsFirstArg
+import static org.mockito.BDDMockito.given
+import static org.mockito.Matchers.any
+import static org.mockito.Matchers.anyObject
+import static org.mockito.Matchers.anyString
+import static org.mockito.Matchers.argThat
+import static org.mockito.Matchers.eq
+import static org.mockito.Mockito.doAnswer
 import static org.mockito.Mockito.when
 
 @RunWith(MockitoJUnitRunner)
@@ -76,12 +88,45 @@ class _3InvocationResponseMockito {
 
 	@Test
 	void shouldOptionallyThrowExceptionBasedOnInputArguments() {
+        //given
+        when(service.singleParamMethod(anyObject())).thenAnswer(throwRuntimeExceptionIfNull())
 
-	}
+        //when
+        try {
+            contrived.singleParamDelegate(null)
+            fail()
+        } catch (RuntimeException ex) {}
+    }
 
-	@Test
+    def Answer throwRuntimeExceptionIfNull() {
+        new Answer<String>() {
+            @Override
+            String answer(InvocationOnMock invocation) throws Throwable {
+                Object arg = invocation.getArguments()[0]
+                if (arg == null) throw new RuntimeException()
+            }
+        }
+    }
+
+    @Test
 	void shouldUseArgumentsToCalculateResponse() {
+        //given
+        when(service.singleParamMethod(anyString())).thenAnswer(useInputInReturn())
 
-	}
+        //when
+        String value = contrived.singleParamDelegate("object")
+
+        //then
+        assert value == "object-response"
+    }
+
+    def Answer useInputInReturn() {
+        new Answer<String>() {
+            @Override
+            String answer(InvocationOnMock invocation) throws Throwable {
+                return "${invocation.getArguments()[0]}-response" as String
+            }
+        }
+    }
 
 }
